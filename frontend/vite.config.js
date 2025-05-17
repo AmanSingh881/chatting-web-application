51e8187628a8f5fb96da811682582d8e3db268c9
ispocktechteam <ispocktechteam@gmail.com>
2025-05-17 17:43:37 +0530

    new changes for cicd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Ensures assets load from the root
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
+});
